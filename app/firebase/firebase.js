73f15196b8314ff13fc9021927840234e2a70883
elmardeveducation <elmarh@deveducation.com>
2020-05-07 22:56:26 +0400

    clean api.js

--- app/firebase/firebase.js
+++ app/firebase/firebase.js
@@ -2,6 +2,9 @@ import firebase from '@react-native-firebase/app';
 
 import {firebaseConfig} from './api';
 
+// For exclude warning in LOGS please use this conditional initializing
 export const db = () => {
-  firebase.initializeApp(firebaseConfig);
+  !firebase.apps.length
+    ? firebase.initializeApp(firebaseConfig)
+    : firebase.app();
 };
